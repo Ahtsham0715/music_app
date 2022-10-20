ae150af01d5eaa3046b956fe8e5f668b517f59dc
Ahtsham0715 <ahtsham50743@gmail.com>
2022-10-20 21:38:44 +0500

    fixes

--- lib/music pages/categories_ui.dart
+++ lib/music pages/categories_ui.dart
@@ -5,95 +5,140 @@ class CategoriesUi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        const Text("Title 1"),
-        GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 5,
-            crossAxisSpacing: 5.0,
-            mainAxisSpacing: 5.0,
-          ),
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {},
-              onHover: ((ishovering) {
-                if (ishovering) {
-                  print('hovering');
-                } else {
-                  print('not hovering');
-                }
-              }),
-              child: Container(
-                margin: const EdgeInsets.all(2.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.blueGrey,
-                ),
-                child: Text("index: $index"),
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text("Title $index"),
+            ),
+            GridView.builder(
+              padding: const EdgeInsets.all(10.0),
+              physics: const AlwaysScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+                crossAxisSpacing: 5.0,
+                mainAxisSpacing: 5.0,
               ),
-            );
-          },
-        ),
-        const Text("Title 2"),
-        GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 5,
-            crossAxisSpacing: 5.0,
-            mainAxisSpacing: 5.0,
-          ),
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              color: Colors.blue,
-              child: Text("index: $index"),
-            );
-          },
-        ),
-        const Text("Title 3"),
-        GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 5,
-            crossAxisSpacing: 5.0,
-            mainAxisSpacing: 5.0,
-          ),
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              color: Colors.blue,
-              child: Text("index: $index"),
-            );
-          },
-        ),
-        const Text("Title 4"),
-        GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 5,
-            crossAxisSpacing: 5.0,
-            mainAxisSpacing: 5.0,
-          ),
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              color: Colors.blue,
-              child: Text("index: $index"),
-            );
-          },
-        )
-      ],
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  onHover: ((ishovering) {
+                    if (ishovering) {
+                      print('hovering');
+                    } else {
+                      print('not hovering');
+                    }
+                  }),
+                  child: Container(
+                    margin: const EdgeInsets.all(2.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.blueGrey,
+                        image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc6U4X9ugWzyY5xqJJeYJroGruDh5Cq325szRQiI7e8R0zZ1ODOvGsuYtE3jbI9PiaP4U&usqp=CAU'))),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+      // children: <Widget>[
+      //   const Text("Title 1"),
+      //   GridView.builder(
+      //     padding: const EdgeInsets.all(10.0),
+      //     physics: const AlwaysScrollableScrollPhysics(),
+      //     shrinkWrap: true,
+      //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //       crossAxisCount: 5,
+      //       crossAxisSpacing: 5.0,
+      //       mainAxisSpacing: 5.0,
+      //     ),
+      //     itemCount: 10,
+      //     itemBuilder: (context, index) {
+      //       return InkWell(
+      //         onTap: () {},
+      //         onHover: ((ishovering) {
+      //           if (ishovering) {
+      //             print('hovering');
+      //           } else {
+      //             print('not hovering');
+      //           }
+      //         }),
+      //         child: Container(
+      //           margin: const EdgeInsets.all(2.0),
+      //           decoration: BoxDecoration(
+      //             borderRadius: BorderRadius.circular(10.0),
+      //             color: Colors.blueGrey,
+      //           ),
+      //           child: Text("index: $index"),
+      //         ),
+      //       );
+      //     },
+      //   ),
+      //   const Text("Title 2"),
+      //   GridView.builder(
+      //     padding: const EdgeInsets.all(10.0),
+      //     physics: const NeverScrollableScrollPhysics(),
+      //     shrinkWrap: true,
+      //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //       crossAxisCount: 5,
+      //       crossAxisSpacing: 5.0,
+      //       mainAxisSpacing: 5.0,
+      //     ),
+      //     itemCount: 10,
+      //     itemBuilder: (context, index) {
+      //       return Container(
+      //         color: Colors.blue,
+      //         child: Text("index: $index"),
+      //       );
+      //     },
+      //   ),
+      //   const Text("Title 3"),
+      //   GridView.builder(
+      //     padding: const EdgeInsets.all(10.0),
+      //     physics: const NeverScrollableScrollPhysics(),
+      //     shrinkWrap: true,
+      //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //       crossAxisCount: 5,
+      //       crossAxisSpacing: 5.0,
+      //       mainAxisSpacing: 5.0,
+      //     ),
+      //     itemCount: 10,
+      //     itemBuilder: (context, index) {
+      //       return Container(
+      //         color: Colors.blue,
+      //         child: Text("index: $index"),
+      //       );
+      //     },
+      //   ),
+      //   const Text("Title 4"),
+      //   GridView.builder(
+      //     padding: const EdgeInsets.all(10.0),
+      //     physics: const NeverScrollableScrollPhysics(),
+      //     shrinkWrap: true,
+      //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //       crossAxisCount: 5,
+      //       crossAxisSpacing: 5.0,
+      //       mainAxisSpacing: 5.0,
+      //     ),
+      //     itemCount: 10,
+      //     itemBuilder: (context, index) {
+      //       return Container(
+      //         color: Colors.blue,
+      //         child: Text("index: $index"),
+      //       );
+      //     },
+      //   )
+      // ],
     );
   }
 }
